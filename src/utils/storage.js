76953cade74dadb40a301f5154accbe77432da21
levon <levon.gambaryan@gmail.com>
2019-10-24 13:52:18 +0300

    small fix to cookies

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -6,10 +6,14 @@ import {
 } from '../store/constants';
 
 // Auto language
-Cookies.set(COOKIES.LANG.name, AUTO_LANG, { expires: COOKIES.LANG.expires });
+if (typeof(AUTO_LANG) !== 'undefined') {
+  Cookies.set(COOKIES.LANG.name, AUTO_LANG, { expires: COOKIES.LANG.expires });
+}
 
 export const rememberLanguage = language => {
-  Cookies.set(COOKIES.LANG.name, language, { expires: COOKIES.LANG.expires });
+  if (typeof(language) !== 'undefined') {
+    Cookies.set(COOKIES.LANG.name, language, { expires: COOKIES.LANG.expires });
+  }
 };
 
 export default Storage;
